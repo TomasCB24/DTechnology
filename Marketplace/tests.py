e68d68ea9e47d6d780c61d22f00d1b71127049c4
juradosalado <juansaladous@gmail.com>
2022-11-27 12:27:51 +0100

    chore(#041): Apply suggestions made by Ezequiel

--- Marketplace/tests.py
+++ Marketplace/tests.py
@@ -5,20 +5,20 @@ from Marketplace.models import Payment
 # Create your tests here.
 class PaymentTestCase(TestCase):
     def setUp(self):
-        Payment.objects.create(amount=3.00)
+        self.payment = Payment.objects.create(amount=3.00)
     
     def test_payment_create(self):
-        payment = Payment.objects.get(amount=3.00)
+        payment = self.payment
         self.assertEqual(payment.amount, 3.00)
     
     def test_payment_update(self):
-        payment = Payment.objects.get(amount=3.00)
+        payment = self.payment
         payment.amount = 4.00
         payment.save()
         self.assertEqual(payment.amount, 4.00)
     
     def test_payment_delete(self):
-        payment = Payment.objects.get(amount=3.00)
+        payment = self.payment
         payment.delete()
         self.assertEqual(Payment.objects.count(), 0)
     
@@ -34,13 +34,13 @@ class PaymentTestCase(TestCase):
     
     #update test
     def test_payment_update_amount_incorrect(self):
-        payment = Payment.objects.get(amount=3.00)
+        payment = self.payment
         with self.assertRaises(Exception):
             payment.amount = -1
             payment.save()
     
     def test_payment_update_amount_null(self):
-        payment = Payment.objects.get(amount=3.00)
+        payment = self.payment
         with self.assertRaises(Exception):
             payment.amount = None
             payment.save()
