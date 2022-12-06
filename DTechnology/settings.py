1787c2cb60ec38a96fbd49a2b7467ac3f71ac334
Tomás Camero <80246365+TomasCB24@users.noreply.github.com>
2022-12-06 14:22:08 +0100

    chore(main): Add allowed host

--- DTechnology/settings.py
+++ DTechnology/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
