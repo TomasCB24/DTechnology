8b4473f959893bf5aeb1cf46679ce55ca06aeec7
ezegonmac <ezegonmac01@gmail.com>
2022-12-13 15:57:30 +0100

    fix(#078): Fix add product to cart in details. Refactor some views

---
+++ DTechnology/utils.py
@@ -0,0 +1,106 @@
+from Marketplace.models import *
+from django.db.models import Q
+from django.core.paginator import Paginator
+from django.shortcuts import redirect
+from django.contrib import messages
+
+def get_cart_counter(request):
+    if 'nonuser' in request.session:
+        order_products = OrderProduct.objects.filter(session_id=request.session['nonuser']).filter(ordered=False)
+        cart_counter = 0
+        for order_product in order_products:
+            cart_counter += order_product.quantity
+        return cart_counter
+    else:
+        return 0
+
+
+def add_product_to_cart(request):
+    quantity = int(request.POST.get('quantity'))
+    product_id = request.POST.get('product_id')
+
+    product = Product.objects.get(id=product_id)
+    
+    order_product = OrderProduct.objects.filter(product=product).filter(ordered=False, session_id=request.session['nonuser'])
+
+    if not order_product.exists():
+        order_product = OrderProduct.objects.create(product=product, quantity=quantity, session_id=request.session['nonuser'])
+        order_product.save()
+    else:
+        order_product = order_product[0]
+
+        if 0 < (product.inventory - order_product.quantity):
+            order_product.quantity += quantity
+            order_product.save()
+        else:
+            messages.warning(request, 'No hay suficientes ' + product.title + ' en el inventario')
+
+
+def delete_product(request, id):
+    product = OrderProduct.objects.get(id=id)
+    product.delete()
+    return redirect('cart')
+
+
+def reduce_product_quantity(request, id):
+    product = OrderProduct.objects.get(id=id)
+    if(product.quantity > 1):
+        product.quantity -= 1
+        product.save()
+    else:
+        product.delete()
+
+    return redirect('cart')
+
+
+def increase_product_quantity(request, id):
+    orderProduct = OrderProduct.objects.get(id=id)
+    product = orderProduct.product
+    
+    order_products = OrderProduct.objects.filter(product=product)
+    cart_quantity = 0
+    for order_product in order_products:
+        cart_quantity += order_product.quantity
+    
+    if 0 < (product.inventory - cart_quantity):
+        orderProduct.quantity += 1
+        orderProduct.save()
+    else:
+        messages.warning(request, 'No hay suficientes ' + product.title + ' en el inventario')
+    return redirect('cart')
+
+
+def get_products(category, department, producer, search, request, page_size):
+    
+    listOfList = []
+
+    if category == 'Cualquier Categoría':
+        category = ''
+    if department == 'Cualquier Departamento':
+        department = ''
+    if producer == 'Cualquier Fabricante':
+        producer = ''
+    
+    productos = Product.objects.filter(section__icontains=category, 
+                                        department__icontains=department, 
+                                        producer__icontains=producer).filter(Q(title__icontains=search) |
+                                                                            Q(section__icontains=search) | 
+                                                                            Q(department__icontains=search) | 
+                                                                            Q(producer__icontains=search))   
+
+    paginator = Paginator(productos, page_size) # Show 5 products per page.
+    page_number = request.GET.get('page')
+    products_page = paginator.get_page(page_number)
+    i=0
+    listaCuatroProductos = []
+    for producto in products_page:
+        if i == 4:
+            listOfList.append(listaCuatroProductos)
+            listaCuatroProductos = []
+            i=0
+        listaCuatroProductos.append(producto)
+        i+=1
+    if len(listaCuatroProductos) <= 4 and len(productos)!=0:
+        listOfList.append(listaCuatroProductos)  
+    
+    return [listOfList, products_page]
--- DTechnology/views.py
+++ DTechnology/views.py
@@ -1,33 +1,20 @@
-from datetime import datetime
 from django.shortcuts import render, redirect
 from Marketplace.models import *
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.decorators.csrf import csrf_exempt
-from django.db.models import Q
 from django.contrib import messages
 from .forms import AddressForm
 from django.http import HttpResponseRedirect
 from django_countries import countries
-from django.core.paginator import Paginator
+from DTechnology.utils import *
 
 from Marketplace.models import Product, CATEGORY_CHOICES, DEPARTMENT_CHOICES, PRODUCER_CHOICES
-
-def get_cart_counter(request):
-    if 'nonuser' in request.session:
-        order_products = OrderProduct.objects.filter(session_id=request.session['nonuser']).filter(ordered=False)
-        cart_counter = 0
-        for order_product in order_products:
-            cart_counter += order_product.quantity
-        return cart_counter
-    else:
-        return 0
 
 def index(request):
     return render(request, 'base_INDEX.html', {'cart_counter': get_cart_counter(request)})
 
 def cart(request):
-
     product_orders = OrderProduct.objects.filter(session_id=request.session['nonuser']).filter(ordered=False)
     # get the total price of the order
     total_price = 0
@@ -35,38 +22,6 @@ def cart(request):
         total_price += product_order.get_final_price()
 
     return render(request, 'base_CART.html', {'products': product_orders, 'total_price': total_price, 'cart_counter': get_cart_counter(request)})
-
-def reduce_product_quantity(request, id):
-    product = OrderProduct.objects.get(id=id)
-    if(product.quantity > 1):
-        product.quantity -= 1
-        product.save()
-    else:
-        product.delete()
-
-    return redirect('cart')
-
-def increase_product_quantity(request, id):
-    orderProduct = OrderProduct.objects.get(id=id)
-    product = orderProduct.product
-    
-    order_products = OrderProduct.objects.filter(product=product)
-    cart_quantity = 0
-    for order_product in order_products:
-        cart_quantity += order_product.quantity
-    
-    if 0 < (product.inventory - cart_quantity):
-        orderProduct.quantity += 1
-        orderProduct.save()
-    else:
-        messages.warning(request, 'No hay suficientes ' + product.title + ' en el inventario')
-    return redirect('cart')
-
-
-def delete_product(request, id):
-    product = OrderProduct.objects.get(id=id)
-    product.delete()
-    return redirect('cart')
 
 def home(request):
 
@@ -85,25 +40,8 @@ def home(request):
             active_category, active_department, active_producer = category, department, producer
             
         elif 'add_to_cart' in request.POST:
-            quantity = int(request.POST.get('quantity'))
-            product_id = request.POST.get('product_id')
-
-            product = Product.objects.get(id=product_id)
-
-            order_product = OrderProduct.objects.filter(product=product).filter(ordered=False, session_id=request.session['nonuser'])
-
-            if not order_product.exists():
-                order_product = OrderProduct.objects.create(product=product, quantity=quantity, session_id=request.session['nonuser'])
-                order_product.save()
-            else:
-                order_product = order_product[0]
-
-                if 0 < (product.inventory - order_product.quantity):
-                    order_product.quantity += 1
-                    order_product.save()
-                else:
-                    messages.warning(request, 'No hay suficientes ' + product.title + ' en el inventario')
-
+            add_product_to_cart(request)
+        
     if request.GET.get('search'):
         search = request.GET.get('search')
     if request.GET.get('category'):
@@ -130,52 +68,6 @@ def home(request):
             'search': search,
             }
         )
-
-def add_to_cart(request,product_id, quantity):
-
-    try:
-        product = Product.objects.get(id=product_id)
-        order_product = OrderProduct.objects.get(product=product, session_id=request.session['nonuser']).filter(ordered=False)
-
-        order_product.add_products(quantity)
-    except:
-        OrderProduct.objects.create(product=product, quantity=quantity, session_id = request.session['nonuser'])
-
-def get_products(category, department, producer, search, request, page_size):
-    
-    listOfList = []
-
-    if category == 'Cualquier Categoría':
-        category = ''
-    if department == 'Cualquier Departamento':
-        department = ''
-    if producer == 'Cualquier Fabricante':
-        producer = ''
-    
-    productos = Product.objects.filter(section__icontains=category, 
-                                        department__icontains=department, 
-                                        producer__icontains=producer).filter(Q(title__icontains=search) |
-                                                                            Q(section__icontains=search) | 
-                                                                            Q(department__icontains=search) | 
-                                                                            Q(producer__icontains=search))   
-
-    paginator = Paginator(productos, page_size) # Show 5 products per page.
-    page_number = request.GET.get('page')
-    products_page = paginator.get_page(page_number)
-    i=0
-    listaCuatroProductos = []
-    for producto in products_page:
-        if i == 4:
-            listOfList.append(listaCuatroProductos)
-            listaCuatroProductos = []
-            i=0
-        listaCuatroProductos.append(producto)
-        i+=1
-    if len(listaCuatroProductos) <= 4 and len(productos)!=0:
-        listOfList.append(listaCuatroProductos)  
-    
-    return [listOfList, products_page]
-
 
 def order(request):
     
@@ -272,22 +164,7 @@ def detail(request,id):
     pro = Product.objects.get(id=id)
     
     if request.method == 'POST' and 'add_to_cart' in request.POST:
-        
-        quan= int(request.POST.get('quantity'))
-        pro_id = request.POST.get('product_id')
-
-        pro = Product.objects.get(id=pro_id)
-
-        order_products = OrderProduct.objects.filter(product=pro)
-
-        cart_quan = 0
-        for ord_pro in order_products:
-            cart_quan += ord_pro.quantity
-        
-        if((pro.inventory - cart_quan) >= quan):
-            add_to_cart(request,pro_id, quan)
-        else:
-            messages.warning(request, 'No hay suficientes ' + pro.title + ' en el inventario')
+        add_product_to_cart(request)
     
     return render(request, 'base_DETAILS.html', 
                             {'product': pro,
@@ -305,4 +182,3 @@ def terms(request):
 
 def privacy(request):
     return render(request, 'base_PRIVACY.html', {'cart_counter': get_cart_counter(request)})
-
